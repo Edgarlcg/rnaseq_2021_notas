2e462a648a4530709d28c388ff3a47f8a078110a
Edgarlcg <jlealriv@lcg.unam.mx>
2021-02-23 12:08:44 -0600

    Notas de la primer sesión

---
+++ R/01-notas.R
@@ -0,0 +1,30 @@
+'''
+NOTAS
+
+INTRODUCCIÓN
+
+Library::Function
+#Los dos puntos extrae la función de cierta libreria
+
+#Es recomendable poner las librerias utilizadas al inicio del script
+
+#En rStudio, en la interfaz, la varita ayuda a hacer el codigo más legible
+
+#En rStudio con al puedes seleccionar un cursos fusionado
+
+Links de interés
+#https://github.com/ComunidadBioInfo/cdsb2019/blob/master/05-fp.pdf
+#https://r-graphics.org/
+
+Para crear un proyecto
+#usethis::create_project("Su_Usuario.github.io")
+
+Para crear un directorio
+#makedir()
+
+Start a setup file
+#usethis::use_r("01-notas.R")
+
+
+
+'''
