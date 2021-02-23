64dadc1a7783653601243a8ca9b482cd9451bbf9
Edgarlcg <jlealriv@lcg.unam.mx>
2021-02-23 12:13:16 -0600

    Notas de Configuración de GitHub

--- R/01-notas.R
+++ R/01-notas.R
@@ -28,3 +28,27 @@ Start a setup file
 
 
 '''
+## Para poder conectar tu compu con GitHub
+usethis::create_github_token() ## Abrirá una página web, escoje un nombre único
+## y luego da click en el botón verde al final. Después copia el token
+## (son 40 caracteres)
+gitcreds::gitcreds_set() ## Ojo, copia el token, no tu password de git!
+## Si no, terminaras en la situación descrita en
+## https://github.com/r-lib/usethis/issues/1347
+## Tambien puedes usar
+# usethis::edit_r_environ()
+## y luego agregar las siguientes dos líneas (la línea en blanco es importante)
+# GITHUB_PAT=TU_CLAVE_DE_40_LETRAS
+#
+
+## Configura tu usuario de GitHub
+usethis::edit_git_config()
+# [user]
+#   name = Leonardo Collado Torres
+#   email = lcolladotor@gmail.com
+
+## Para inicializar el repositorio de Git
+usethis::use_git()
+
+## Para conectar tu repositorio local de Git con los servidores de GitHub
+usethis::use_github()
